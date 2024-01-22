2a94414691fc054f2abb9fa9d8a51f798ca68c4e
Skypix <skypix9player@gmail.com>
2024-01-22 11:58:45 +0100

    modified:   app/routes/_index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -14,6 +14,8 @@ export async function loader({context}) {
 
 export default function Index() {
   const {collections} = useLoaderData();
+
+  console.log(collections.edges[0].node.title);
   return (
     <section className="w-full gap-4">
       <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
